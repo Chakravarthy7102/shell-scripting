447825f775512870c4865764c5074b32add1b7c6
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2022-01-28 22:39:21 +0530

    ao

--- src/arithmetic_operations.sh
+++ src/arithmetic_operations.sh
@@ -6,28 +6,28 @@ Bash does not support number data type. Let’s see different
 mechanisms through which we can perform arithmetic operations.
 '
 
-# let RESULT=1+1
+let RESULT=1+1
 
-# echo $RESULT
-# # result : 2
-
-
-# let a=10
-# let b=20
-# let result="a + b"
-
-# echo $result
-
-# # expr command will redirect the output onto cli
-# expr 1 + 1
+echo $RESULT
+# result : 2
 
 
-# echo -n "Enter a number:"
-# read a
-# echo -n "Enter a number:"
-# read b
-# sum=$(( a+b ))
-# echo "sum="$sum
+let a=10
+let b=20
+let result="a + b"
+
+echo $result
+
+# expr command will redirect the output onto cli
+expr 1 + 1
+
+
+echo -n "Enter a number:"
+read a
+echo -n "Enter a number:"
+read b
+sum=$(( a+b ))
+echo "sum="$sum
 
 num=5
 (( num+=num ))
